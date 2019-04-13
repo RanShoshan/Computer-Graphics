bccdc8d600caaa5c797d971fb23b80f66f0f2f39
RanShoshan <ranshoransho@gmail.com>
2019-04-13 11:18:21 +0300

    circle added

--- ComputerGraphics/ComputerGraphics/MainWindow.xaml.cs
+++ ComputerGraphics/ComputerGraphics/MainWindow.xaml.cs
@@ -21,23 +21,59 @@ namespace ComputerGraphics
         NONE = 0,
         BTN_LINE_1ST_CLICK = 1,
         BTN_LINE_2ST_CLICK = 2,
+        BTN_CIRCLE_1ST_CLICK ,
+        BTN_CIRCLE_2ST_CLICK ,
     }
 
     public partial class MainWindow : Window
     {
         public UserState state = UserState.NONE;
         public Point lastPoint = new Point();
+
         public MainWindow()
         {
             InitializeComponent();
-  
-            
         }
+        
+
+
+
+        public void OnBtnCircleClicked(object sender, RoutedEventArgs e)
+        {
+
+            state = UserState.BTN_CIRCLE_1ST_CLICK;
+        }
+
+
+        public void Circle(Point p1, Point p2)
+        {
+            int xCenter = Convert.ToInt32(p2.X);
+            int yCenter = Convert.ToInt32(p2.Y);
+            int xr = Convert.ToInt32(p1.X);
+            int yr = Convert.ToInt32(p1.Y);
+
+            var a = (xCenter - xr);
+            var b = (yCenter - yr);
+            var r = Math.Sqrt(a * a + b * b);
+
+            for (double i = 0.0; i < 360.0; i += 0.1)
+            {
+            double angle = i * System.Math.PI / 180;
+            int x = (int)(xr + r * System.Math.Cos(angle));
+            int y = (int)(yr + r * System.Math.Sin(angle));
+                    SetPixel(x, y);
+            }
+
+        }
+        
 
         public void OnBtnLineClicked(object sender, RoutedEventArgs e) {
 
             state = UserState.BTN_LINE_1ST_CLICK;
         }
+
+
+
 
         private static void Swap<T>(ref T lhs, ref T rhs) { T temp; temp = lhs; lhs = rhs; rhs = temp; }
 
@@ -73,6 +109,14 @@ namespace ComputerGraphics
                 case UserState.BTN_LINE_2ST_CLICK:
                     Line(lastPoint, e.GetPosition(myCanvas));
                     state = UserState.BTN_LINE_1ST_CLICK;
+                    break;
+                case UserState.BTN_CIRCLE_1ST_CLICK:
+                    lastPoint = e.GetPosition(myCanvas);
+                    state = UserState.BTN_CIRCLE_2ST_CLICK;
+                    break;
+                case UserState.BTN_CIRCLE_2ST_CLICK:
+                    Circle(lastPoint, e.GetPosition(myCanvas));
+                    state = UserState.BTN_CIRCLE_1ST_CLICK;
                     break;
                 default:
                     break;
